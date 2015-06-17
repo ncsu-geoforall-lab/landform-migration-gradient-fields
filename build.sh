03cf87cebef4c6611b40cff9efb36845ef33ef5d
Helena Mitasova <hmitaso@ncsu.edu>
2015-06-17 10:26:58 -0400

    update title and authors

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 ./build-slides.py --outdir=build \
-    --title="Mapping gradient fields of landform migration using an&nbsp;open science approach" \
-    --meta-description="North Carolina State University, Department of Marine, Earth and Atmospheric Sciences, Graduate Student Seminar" \
-    --meta-author="Vaclav Petras" \
+    --title="Mapping gradient fields of landform migration" \
+    --meta-description="North Carolina State University, Center for Geospatial Analytics and Department of Marine, Earth and Atmospheric Sciences" \
+    --meta-author="Vaclav Petras, Helena Mitasova, Anna Petrasova" \
     title.html intro.html gradients.html outro.html open.html end.html
